992bc0fb7496904a767cecbbc4251f5456dd218b
gajanand1234567 <22ce02008@iitbbs.ac.in>
2026-08-01 19:00:51 +0530

    Fix: remove GET error messages, increase gunicorn timeout to 120s for model loading

--- NST_Code/app.py
+++ NST_Code/app.py
@@ -123,10 +123,11 @@ def index():
             except Exception as e:
                 error = str(e)
     else:
-        if not content_filename:
-            error = 'Please upload content image'
-        if not style_filename:
-            error = 'Please upload style image'
+        if request.method == 'POST':
+            if not content_filename:
+                error = 'Please upload content image'
+            if not style_filename:
+                error = 'Please upload style image'
 
     return render_template('index.html', form=form, result_image=result_image, content_image=content_filename,
                            style_image=style_filename, error=error)
